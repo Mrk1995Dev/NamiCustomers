75004055370e62263b7b02f69fa87b44af79b5c4
Hamed2 Aghamohammadi2 <h.aghamohammadi@IT-AGHAMOHAMMAD>
2025-07-13 10:15:25 +0330

    14040422

--- NamiCustomers.Abstractions/Dtos/Vehicles/VehicleModelDto.cs
+++ NamiCustomers.Abstractions/Dtos/Vehicles/VehicleModelDto.cs
@@ -7,23 +7,30 @@ public class VehicleModelDto
 {
     public int? Id              { get; set; }
     public int SubscriberId { get; set; }
-    public string? VehicleModelName { get; set; }
-    public string? VehicleModelLocalizedName { get; set; }
+	[Display(Name = "مدل خودرو ")]
+	public string? VehicleModelName { get; set; }
+	[Display(Name = "نام خودرو")]
+	public string? VehicleModelLocalizedName { get; set; }
     public string? SelectedVehicleDescription { get; set; }
-    public string? ProductYear { get; set; }
-    public string? BodyColor { get; set; }
-    public string? MotorNumber { get; set;   }
-    public string? FullSystem { get;set; }
+	[Display(Name = "سال ساخت")]
+	public string? ProductYear { get; set; }
+	[Display(Name = "رنگ بدنه")]
+	public string? BodyColor { get; set; }
+	[Display(Name = "شماره موتور")]
+	public string? MotorNumber { get; set;   }
+	[Display(Name = "نوع سوخت")]
+	public string? FullSystem { get;set; }
     public Guid? VehicleModelId { get; set; }
     public string? ChassisUsageTypeName { get; set; }
 
 		public string? SelectedVehicleCommonName { get;set; }
- 
-		/// <summary>
-		/// شماره شاسی 
-		/// </summary>
 
-    public string? VinNumber { get; set; }
+	/// <summary>
+	/// شماره شاسی 
+	/// </summary>
+
+	[Display(Name = "شماره شاسی")]
+	public string? VinNumber { get; set; }
     public Guid? SalePlanIdSevenSoft { get; set; }
     public Guid? SaleBasketIdSevenSoft { get; set; }
     public Guid? VehicleModelIdSevensoft { get; set; }
