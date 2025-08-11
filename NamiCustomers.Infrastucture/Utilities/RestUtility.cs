a77a8261915ab37b3ada3f4d5ac123505eae4883
a.moradi <a.moradi@IT-MORADI>
2025-08-11 16:44:01 +0330

    refac 2

--- NamiCustomers.Infrastucture/Utilities/RestUtility.cs
+++ NamiCustomers.Infrastucture/Utilities/RestUtility.cs
@@ -1,42 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 
-namespace NamiCustomers.Infrastucture.Utilities
+namespace NamiCustomers.Infrastucture.Utilities;
+
+public static class RestUtility
 {
-    public static class RestUtility
+    public static async Task<T> GetData<T>(string baseUrl, string apiAddress, dynamic queryString)
     {
-        public static async Task<T> GetData<T>(string baseUrl, string apiAddress, dynamic queryString)
+        if (!string.IsNullOrEmpty(baseUrl))
         {
-            if (!string.IsNullOrEmpty( baseUrl))
-            {
-                apiAddress = $"{baseUrl}/{apiAddress}";
-            }
-            using HttpClient client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, $"{apiAddress}{queryString}");
-            request.Headers.Add("Accept", "application/json");
-            var response = await client.SendAsync(request);
-            string content = await response.Content.ReadAsStringAsync();
-
-            return await Task.FromResult(JsonSerializer.Deserialize<T>(content));
+            apiAddress = $"{baseUrl}/{apiAddress}";
         }
-
-
-
-        public static async Task<T> PostData<T>(string apiAddress, dynamic queryModel)
+        using HttpClient client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{apiAddress}{queryString}");
+        request.Headers.Add("Accept", "application/json");
+        var response = await client.SendAsync(request);
+        string responseContent = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
         {
-            string baseUrl = Infrastucture.Properties.Resource7Soft.BaseUrl;
-            using HttpClient client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Post, $"{baseUrl}/{apiAddress}");
-            request.Headers.Add("Accept", "application/json");
-            var content = new StringContent(JsonSerializer.Serialize(queryModel), null, "application/json");
-            request.Content = content;
-            var response = await client.SendAsync(request);
-            string responseContent = await response.Content.ReadAsStringAsync();
             return await Task.FromResult(JsonSerializer.Deserialize<T>(responseContent));
         }
+        return Activator.CreateInstance<T>();//TODO moradi
+    }
+
+
+
+    public static async Task<T> PostData<T>(string baseUrl, string apiAddress, dynamic queryModel)
+    {
+        if (!string.IsNullOrEmpty(baseUrl))
+        {
+            apiAddress = $"{baseUrl}/{apiAddress}";
+        }
+        using HttpClient client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Post, $"{apiAddress}");
+        request.Headers.Add("Accept", "application/json");
+        var content = new StringContent(JsonSerializer.Serialize(queryModel), null, "application/json");
+        request.Content = content;
+        var response = await client.SendAsync(request);
+        string responseContent = await response.Content.ReadAsStringAsync();
+        if (response.IsSuccessStatusCode)
+        {
+            return await Task.FromResult(JsonSerializer.Deserialize<T>(responseContent));
+        }
+        return Activator.CreateInstance<T>();
     }
 }
