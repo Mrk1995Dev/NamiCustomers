3e3a64046766e1a1b01efc7757806d69c63a0f8b
a.moradi <a.moradi@IT-MORADI>
2025-09-07 10:34:31 +0330

    ajax form add

--- NamiCustomers.MVC/Controllers/BookingController.cs
+++ NamiCustomers.MVC/Controllers/BookingController.cs
@@ -21,8 +21,36 @@ namespace NamiCustomers.MVC.Controllers
                 SetError(result.Errors.FirstOrDefault());
                 return RedirectToAction("Index", "Home");
             }
-
+ 
             return View(result.Data);
+        }
+
+        public async Task<IActionResult> Refresh(BookingTurnRequest? bookingTurnRequest)
+        {
+            if (bookingTurnRequest == null)
+            {
+                bookingTurnRequest = new BookingTurnRequest();
+            }
+
+
+            var result = await bookingService.GetBookingTurnAsync(bookingTurnRequest);
+
+            if (!result.Succeeded)
+            {
+                SetError(result.Errors.FirstOrDefault());
+                // Return a JSON result to the client
+                return Json(new
+                {
+                    success = false,
+                    Data = new BookingTurnResponse { SubCountryId = bookingTurnRequest.SubCountryId }
+                });
+            }
+            // Return a JSON result to the client
+            return Json(new
+            {
+                success = true,
+                Data = result.Data
+            });
         }
 
 
