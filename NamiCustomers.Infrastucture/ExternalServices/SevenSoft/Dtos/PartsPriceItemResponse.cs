ec37c7069c6146edaee798f3a30c54b28dcd6527
a.moradi <a.moradi@IT-MORADI>
2025-11-18 15:43:35 +0330

    refac 1

---
+++ NamiCustomers.Infrastucture/ExternalServices/SevenSoft/Dtos/PartsPriceItemResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace NamiCustomers.Infrastucture.ExternalServices.SevenSoft.Dtos;
+
+public class PartsPriceItemResponse
+{
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public bool Active { get; set; }
+    public int SubscriberTypeId { get; set; }
+}
